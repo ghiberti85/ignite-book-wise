ae76134e23a2c476fb6b18b54dc62925577692c9
ghiberti85 <ghiberti85@gmail.com>
2024-05-24 14:27:34 -0300

    feat: Add Global Style

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,59 @@
+export const defaultTheme = {
+    colors: {
+      white: '#FFFFFF',
+      black: '#000000',
+      danger: '#F75A68',
+  
+      green: {
+        100: '#50B2C0',
+        200: '#255D6A',
+        300: '#0A313C',
+      },
+  
+      purple: {
+        100: '#8381D9',
+        200: '#2A2879',
+      },
+  
+      teal: {
+        500: '#7FD1CC',
+      },
+  
+      indigo: {
+        500: '#9694F5',
+        900: '#8381D90F',
+      },
+  
+      gray: {
+        100: '#F8F9FC',
+        200: '#E6E8F2',
+        300: '#D1D6E4',
+        400: '#8D95AF',
+        500: '#303F73',
+        600: '#252D4A',
+        700: '#181C2A',
+        800: '#0E1116',
+        900: '#E6E8F20A',
+      },
+  
+      'gradient-vertical': `linear-gradient(180deg, #7FD1CC 0%, #9694F5 100%)`,
+      'gradient-horizontal': `linear-gradient(90deg, #7FD1CC 0%, #9694F5 100%)`,
+    },
+  
+    lineHeights: {
+      shorter: '125%',
+      short: '140%',
+      base: '160%',
+      tall: '180%',
+    },
+  
+    fontSizes: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      md: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+    },
+  }
+  
