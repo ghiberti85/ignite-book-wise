4c456d26b837b2077a0e7f540585fb718e74d16c
ghiberti85 <ghiberti85@gmail.com>
2024-06-03 23:08:47 -0300

    Latest Book Review Component

--- src/components/LatestBookReview/index.tsx
+++ src/components/LatestBookReview/index.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
-import { Book, BookCardRating, LatestBookReviewContainer } from "./styles";
+import { Book, BookCard, BookCardRating, BookCardUser, LatestBookReviewContainer } from "./styles";
 import { useSession } from "next-auth/react";
 import { useQuery } from "@tanstack/react-query";
 import { api } from "@/lib/axios";
 import { Avatar } from "../Avatar";
+import Link from "next/link";
+import { formatDate } from "date-fns";
 
 interface User {
     id: string
@@ -58,31 +60,44 @@ export function LatestBookReviews() {
             <h2>Avaliações mais recentes</h2>
 `                                                                   Q§Z`
             <div>
-                <DialogBook>
-                    <BookdCard>
-                        <BookCardRating>
-                            <Link>
-                                <Avatar />
-
-                                <BookCardUser>
-                                    <strong></strong>
-                                    <span></span>
-                                </BookCardUser>
-                            </Link>
-
-                            <Rating />
-                        </BookCardRating>
-
-                        <Book>
-                            <Image />
-                            <div>
-                                <h3></h3>
-                                <h4></h4>
-                                <LimitedText />
-                            </div>
-                        </Book>
-                    </BookdCard>
-                </DialogBook>
+                {data?.ratings.map((rating) => (
+                    <DialogBook bookId={rating.book.id} key={rating.id}>
+                        <BookCard>
+                            <BookCardRating>
+                                <Link href={`/profile/${rating.user.id}`}>
+                                    <Avatar 
+                                        size="md" 
+                                        src={rating.user.avatar_url}
+                                        width={40}
+                                        height={40}
+                                        alt={rating.user.name} 
+                                    />
+    
+                                    <BookCardUser>
+                                        <strong>{rating.user.name}</strong>
+                                        <span>{formatDate(new Date(rating.created_at))}</span>
+                                    </BookCardUser>
+                                </Link>
+    
+                                <Rating rate={rating.rate} disabled={true} />
+                            </BookCardRating>
+    
+                            <Book>
+                                <Image 
+                                    src={rating.book.cover_url} 
+                                    alt={rating.book.name}
+                                    width={108}
+                                    height={152}
+                                />
+                                <div>
+                                    <h3>{rating.book.name}</h3>
+                                    <h4>{rating.book.author}</h4>
+                                    <LimitedText text={rating.description} />
+                                </div>
+                            </Book>
+                        </BookCard>
+                    </DialogBook>
+                ))}
             </div>
         </LatestBookReviewContainer>
     )
