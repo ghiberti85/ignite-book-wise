c2653afd3a2a226a2dbc0f4562fdb0fe61a0ba5f
ghiberti85 <ghiberti85@gmail.com>
2024-06-06 23:36:07 -0300

    Starting Rating Component

--- src/components/Rating/index.tsx
+++ src/components/Rating/index.tsx
@@ -1,7 +1,17 @@
 
+interface FormInputValues {
+    rate: number
+    description: string
+}
 
-export function Rating() {
-    return(
-        
-    )
+interface RatingProps {
+    rate?: number
+    disabled?: boolean
+    handleRating?: (rate: number) => void
+    register?: UseFormRegister<FormInputValues>
+    size?: number
+}
+
+export function Rating({ register, disabled, rate=0, handleRating, size= 16 }: RatingProps) {
+    return()
 }
