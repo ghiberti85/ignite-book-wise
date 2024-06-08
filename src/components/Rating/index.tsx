4ee46f0346de5959f62748b6035a70635011a801
ghiberti85 <ghiberti85@gmail.com>
2024-06-08 16:45:42 -0300

    Finish Rating Component

--- src/components/Rating/index.tsx
+++ src/components/Rating/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react"
+import { UseFormRegister } from "react-hook-form"
+import { RatingContainer, StyledStar } from "./styles"
 
 interface FormInputValues {
     rate: number
@@ -12,6 +15,96 @@ interface RatingProps {
     size?: number
 }
 
-export function Rating({ register, disabled, rate=0, handleRating, size= 16 }: RatingProps) {
-    return()
+export function Rating({ 
+    register, 
+    disabled, 
+    rate=0, 
+    handleRating, 
+    size= 16 
+}: RatingProps) {
+    const [rating, setRating] = useState<number>(0)
+    const [hover, setHover] = useState<number>(0)
+    
+    return(
+        <RatingContainer>
+            {disabled && 
+                [...Array(5)].map((_, index) => {
+                    const currentRatingIndex = index + 1
+
+                    return (
+                        <label key={index}>
+                            <input 
+                                type="radio"
+                                name="rating"
+                                value={currentRatingIndex}
+                                onClick={() => {
+                                    setRating(currentRatingIndex)
+                                }}
+                            />
+                            {currentRatingIndex <= rate ? (
+                                <StyledStar 
+                                    size={size}
+                                    weight="fill"
+                                    isRated={currentRatingIndex <= rate}
+                                />
+                            ) : (
+                                <StyledStar 
+                                    size={size}
+                                    weight="fill"
+                                    isRated={currentRatingIndex <= rate}
+                                />
+                            )}
+                        </label>
+                    )
+                })}
+            {!disabled &&
+                [...Array(5)].map((_, index) => {
+                    const currentRatingIndex = index + 1
+                    
+                    return (
+                        <label key={index}>
+                            <input 
+                                {...(register && register('rate'))}
+                                type="radio"
+                                name="rating"
+                                value={currentRatingIndex}
+                                onClick={() => {
+                                    setRating(currentRatingIndex)
+                                    handleRating && handleRating(currentRatingIndex)
+                                }}
+                            />
+                            {currentRatingIndex <= rating || currentRatingIndex <= hover ? (
+                                <StyledStar 
+                                    size={size}
+                                    weight="fill"
+                                    isRated={
+                                        currentRatingIndex <= rating && currentRatingIndex <= hover
+                                    }
+
+                                    onMouseEnter={() => {
+                                        setHover(currentRatingIndex)
+                                    }}
+                                    onMouseLeave={() => setHover(0)}
+                                />
+                            ) : (
+                                <StyledStar 
+                                    size={size}
+                                    fill="outline"
+                                    isRated={
+                                        currentRatingIndex <= rating && currentRatingIndex <= hover
+                                    }
+
+                                    onMouseEnter={() => {
+                                        setHover(currentRatingIndex)
+                                    }}
+
+                                    onMouseLeave={() => setHover(0)}
+                                />
+                            )}
+                        </label>
+                    )
+                })
+            }
+        </RatingContainer>
+    )
 }
