4379d0ad0480b8a91f29168333e2c805e8f6880f
ghiberti85 <ghiberti85@gmail.com>
2024-06-10 23:25:58 -0300

    Prisma Adapter: linkAccount method

--- src/lib/auth/prisma-adapter.ts
+++ src/lib/auth/prisma-adapter.ts
@@ -93,5 +93,23 @@ export function PrismaAdapter(
                 emailVerified: null,
             }
         },
+
+        async linkAccount(account) {
+            await prisma.account.create({
+                data: {
+                    user_id: account.userId,
+                    type: account.type,
+                    provider: account.provider,
+                    provider_account_id: account.providerAccountId,
+                    refresh_token: account.refresh_token,    
+                    access_token: account.access_token,
+                    expires_at: account.expires_at,
+                    token_type: account.token_type,
+                    scope: account.scope,
+                    id_token: account.id_token,
+                    session_state: account.session_state,
+                },
+            })
+        },
     }
 }
